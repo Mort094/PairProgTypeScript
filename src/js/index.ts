b0dc58898439d26d419eb7e1f732bf2c35943367
Morten Jakobsen Møller <mort748a@edu.easj.dk>
2020-10-22 12:41:02 +0200

    TypeScript

--- src/js/index.ts
+++ src/js/index.ts
@@ -1,11 +1,26 @@
+import axios, {
+    AxiosResponse,
+    AxiosError 
+} from "../../node_modules/axios/index"
+
+interface ICd{
+    id: number
+    title: string
+    artist: string
+    duration: number
+    numberOfTracks: number
+    yearOfRelease: number
+}
+let baseUrl: string = "http://mort-rest.azurewebsites.net/api/CD"
 new Vue({
     // TypeScript compiler complains about Vue because the CDN link to Vue is in the html file.
     // Before the application runs this TypeScript file will be compiled into bundle.js
     // which is included at the bottom of the html file.
     el: "#app",
     data: {
-        name: "",
-        greeting: ""
+        cd: []
+     
+        
     },
     methods: {
         sayHello() {
